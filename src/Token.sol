07e18d205a0edda457248acda17caf3693eca0a8
cicero <cicerog.silvestre@gmail.com>
2025-07-03 20:29:13 -0300

    Criada funcao approve e transferFrom

--- src/Token.sol
+++ src/Token.sol
@@ -6,23 +6,37 @@ contract Token {
     string public symbol;
     uint8 public decimal;
     uint8 public totalSupply;
+
     mapping(address => uint256) balance;
+    mapping(address => mapping(address => uint256)) allowance;
 
     event Transfer(address from, address to, uint256 amount);
+    event Approval(address _owner, address _spender, uint256 amount);
 
     function balanceOf(address owner) public view returns (uint256) {
         uint256 balanceOfUser = balance[owner];
         return balanceOfUser;
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool) {
-        require(balance[msg.sender] >= 0, "FALTA GRANA");
+    function transfer(address _to, uint256 _value) public {
+        require(balance[msg.sender] >= 0, "INSUFFICIENT_AMOUNT");
 
         balance[_to] += _value;
         balance[msg.sender] -= _value;
 
         emit Transfer(msg.sender, _to, _value);
+    }
 
-        return true;
+    function approve(address _spender, uint256 _value) public {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public {
+        require(
+            allowance[_from][msg.sender] >= _value,
+            "INSUFFICIENT_ALLOWNCE"
+        );
     }
 }
